a1a33049855d36cece4bbdaa649fa2620854840a
betir <betirov95@gmail.com>
2022-03-03 11:48:27 +0300

    fix

--- DelegateAndEventsExample/Program.cs
+++ DelegateAndEventsExample/Program.cs
@@ -8,38 +8,14 @@ namespace DelegateAndEventsExample
 {
     internal class Program
     {
-        public delegate void Del(string msg);
-        public delegate void Del2(int num);
+     
 
         static void Main(string[] args)
         {
-            Del _delate = PrintMessage;
-            /// Коммент
-
-            Del2 _delate2 = PrintNum;
-
-            _delate("Work");
-            _delate("This is delegate method");
-
-            DelegateParam(_delate);
-
-            Console.ReadKey();
+        
         }
 
 
-        public static void PrintNum(int a)
-        {
-            Console.WriteLine(a);
-        }
-
-        public static void DelegateParam(Del del)
-        {
-            del("Delegate with method param");
-        }
-
-        public static void PrintMessage(String msg)
-        {
-            Console.WriteLine(msg);
-        }
+  
     }
 }
